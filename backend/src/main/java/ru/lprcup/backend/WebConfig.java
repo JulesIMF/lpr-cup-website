6a4929741f105f5673d2ffbca891671020b0d48f
Ivan Doroshenko <julesimf@yandex.ru>
2023-12-17 00:19:05 +0300

    [BACK] Fixup a bit

--- backend/src/main/java/ru/lprcup/backend/WebConfig.java
+++ backend/src/main/java/ru/lprcup/backend/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        System.out.print("Added\n");
         registry.addResourceHandler("/**") // URL путь к ресурсам
                 .addResourceLocations("classpath:/static/"); // Физический путь к ресурсам в проекте
     }
--- backend/src/main/java/ru/lprcup/backend/controllers/HtmlController.java
+++ backend/src/main/java/ru/lprcup/backend/controllers/HtmlController.java
@@ -1,7 +1,6 @@
 package ru.lprcup.backend.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.util.RouteMatcher.Route;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
